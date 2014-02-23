2c9388a05ad421c1f2b03bd935ae1daf2d301135
Aadit M Shah <aaditmshah@myopera.com>
2014-02-23 09:50:42 +0530

    Revert "Bumped to version 4.1.0 - fixes #7"
    This reverts commit d3a128fb8d6352304dba67ea3a982f1ec3effca6.

--- augment.js
+++ augment.js
@@ -5,11 +5,16 @@
 }(this, function () {
     "use strict";
 
-    var Factory = function () {}, slice = Array.prototype.slice;
+    var Factory = function () {};
+    var slice = Array.prototype.slice;
 
     return function (base, body) {
-        var uber = Factory.prototype = typeof base === "function" ? base.prototype : base, prototype = new Factory;
-        body.apply(prototype, slice.call(arguments, 2).unshift(uber));
+        var 
+        uber = Factory.prototype = typeof base === "function" ? base.prototype : base,
+        prototype = new Factory,
+        args = slice.call(arguments, 2);
+        args.unshift(uber);
+        body.apply(prototype, args);
         if (!prototype.hasOwnProperty("constructor")) return prototype;
         var constructor = prototype.constructor;
         constructor.prototype = prototype;
