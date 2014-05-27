dd71139d9755ced67895825a5cba0ad5215b448a
Aadit M Shah <aaditmshah@fastmail.fm>
2014-05-27 08:55:11 +0530

    Added utility functions `defclass` and `extend`.

--- augment.js
+++ augment.js
@@ -8,7 +8,7 @@
     var Factory = function () {};
     var slice = Array.prototype.slice;
 
-    return function (base, body) {
+    var augment = function (base, body) {
         var uber = Factory.prototype = typeof base === "function" ? base.prototype : base;
         var prototype = new Factory;
         body.apply(prototype, slice.call(arguments, 2).concat(uber));
@@ -16,5 +16,20 @@
         var constructor = prototype.constructor;
         constructor.prototype = prototype;
         return constructor;
-    }
+    };
+
+    augment.defclass = function (prototype) {
+        var constructor = prototype.constructor;
+        constructor.prototype = prototype;
+        return constructor;
+    };
+
+    augment.extend = function (base, body) {
+        return augment(base, function (uber) {
+            for (var key in body) this[key] = body[key];
+            this.uber = uber;
+        });
+    };
+
+    return augment;
 }));
